760f212d7acb5c082c1f5e3f809dd70e2997ae9b
Brooke Moss <me@brookem.dev>
2023-11-05 20:10:28 -0500

    Comments main script

--- row_detect.py
+++ row_detect.py
@@ -1,22 +1,40 @@
 import cv2 as cv
 import random
-from row_helpers import binary_simple, binary_adaptive, find_best_counts, draw_peak_lines
+from row_helpers import binary_simple, find_best_counts, draw_peak_lines
 
-image_number = random.randint(1, 281)
-img = cv.imread(f"./data/CRBD/Images/crop_row_{image_number:03}.JPG", flags=cv.IMREAD_COLOR)
+# Option 1: Use specific image file in ./data directory
+filename = "crop_row_258.JPG"
 
+# Option 2: Use random image from CRBD dataset if filename not specified
+if filename == "":
+    filename = f"crop_row_{random.randint(1, 281):03}.JPG"
+    filepath = f"./data/CRBD/Images/{filename}"
+else:
+    filepath = f"./data/{filename}"
+
+# Read image file into OpenCV/numpy formal
+img = cv.imread(filepath, flags=cv.IMREAD_COLOR)
+
+# Convert to HSV and calculate binary thresholded image (find green pixels)
 binary_img = binary_simple(img)
 
+# Calculate height and width of image
 (height, width) = binary_img.shape[:2]
 
-# mask = np.zeros((height, width))
-
+# Find best row estimate for each bottom pixel
 top, count = find_best_counts(binary_img)
 
+# Find best estimates for rows and add to image
 draw_peak_lines(img, count, top, height)
 
+# Display image with row estimates
 cv.imshow("Display window", img)
+
+# Display binary image
 cv.imshow("hsv_binary window", binary_img)
-# cv.imshow("mask", mask)
+
+# Export images
+cv.imwrite(f"./data/outputs/{filename}", img)
+cv.imwrite(f"./data/outputs/binary_{filename}", binary_img)
 
 k = cv.waitKey(0)
